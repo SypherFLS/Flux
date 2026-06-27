026e42d400df9692d934a73f1380cca7e0b3f8e1
SypherFLS <www.krasavinlv@gmail.com>
2026-06-27 23:53:36 +0300

    feat генерация случайного значения рулетки и оркестрация

--- backend/internal/generation/generation.go
+++ backend/internal/generation/generation.go
@@ -4,33 +4,80 @@ import (
 	"math/rand"
 	"FLUX/internal/generation/payback"
 	"time"
-	"fmt"
+	_ "fmt"
 )
 
+var numberToColor = map[int]string{
+	0: "green",
 
-type Roulet interface {
+	1: "red",
+	2: "black",
+	3: "red",
+	4: "black",
+	5: "red",
+	6: "black",
+	7: "red",
+	8: "black",
+	9: "red",
+	10: "black",
+
+	11: "black",
+	12: "red",
+	13: "black",
+	14: "red",
+	15: "black",
+	16: "red",
+	17: "black",
+	18: "red",
+
+	19: "red",
+	20: "black",
+	21: "red",
+	22: "black",
+	23: "red",
+	24: "black",
+	25: "red",
+	26: "black",
+	27: "red",
+	28: "black",
+
+	29: "black",
+	30: "red",
+	31: "black",
+	32: "red",
+	33: "black",
+	34: "red",
+	35: "black",
+	36: "red",
+}
+
+type RouletInter interface {
 	Start()
 	Roll() payback.Payback
 }
 
-var rng = rand.New(rand.NewSource(time.Now().UnixNano()))
 
-func Roll() payback.Payback{
-	// red := []int{1, 3, 5, 7, 9, 12, 14, 16, 18, 21, 23, 25, 27, 28, 30, 32, 34, 36}
-	// black := []int{2, 4, 6, 8, 10, 11, 13, 15, 17, 19, 20, 22, 24, 26, 29, 31, 33, 35}
-	// green := []int{0}
+func StartRoller() {
+	var rng = rand.New(rand.NewSource(time.Now().UnixNano()))
+	go func() {
+		ticker := time.NewTicker(3 * time.Minute)
+		defer ticker.Stop()
 
-	colors := []string{"red", "black", "green"}
-
-	var pb payback.Payback 
-
-	seed := time.Now().UnixNano()
-	fmt.Println(seed)
-
-	pb.Color = colors[rng.Intn(len(colors))]
-
-	return pb
+		for {
+			Roll(rng)
+			<-ticker.C
+		}
+	}()
 }
 
 
+func Roll(rng *rand.Rand) payback.Payback{
+	var pb payback.Payback
+
+	num := rng.Intn(37)
+	pb.Number = num
+	pb.Color = numberToColor[num]
+
+	return pb
+}
 
